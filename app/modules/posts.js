8008886b3067aaa56ca5aae10a4a5e11f35e8918
jiangwanwei <424394863@qq.com>
2018-05-18 23:56:02 +0800

    add posts list

--- app/modules/posts.js
+++ app/modules/posts.js
@@ -1,4 +1,51 @@
 const {Post} = require('../lib/mongo')
+const marked = require('marked')
+
+// 将 post 的 content 从 markdown 转换成 html
+Post.plugin('contentToHtml', {
+    afterFind: results => results.map(post => {
+        post.content = marked(post.content)
+        return post
+    }),
+    afterFindOne: result => {
+        if (result) {
+            result.content = marked(result.content)
+        }
+        return result
+    }
+})
+
 module.exports = {
+    // 创建一片文章
     create: post => Post.create(post).exec(),
+    // 通过文章 id 获取一篇文章
+    getPostById: _id => {
+        return Post
+            .findOne({ _id })
+            .populate({ path: 'author', model: 'User' })
+            .addCreateAt()
+            .contentToHtml()
+            .exec()
+    },
+    // 按创建时间降序获取所有用户文章或者某个特定用户的所有文章
+    getPosts: author => {
+        const query = {}
+        if (author) {
+            query.author = author
+        }
+        return Post
+            .find(query)
+            .populate({ path: 'author', model: 'User' })
+            .sort({ _id: -1 })
+            .addCreateAt()
+            .contentToHtml()
+            .exec()
+
+    },
+    // 通过文章 id 给 pv 加 1
+    incPv: _id => {
+        return Post
+            .update({ _id }, { $inc: { pv: 1 }})
+            .exec()
+    }
 }
--- app/routes/posts.js
+++ app/routes/posts.js
@@ -1,15 +1,46 @@
 const router = require('express').Router()
 const {checkLogin} = require('../middlewares/auth')
 
+const PostModel = require('../modules/posts')
+
 // GET /posts 所有用户或者特定用户的文章页
 //   eg: GET /posts?author=xxx
 router.get('/', function (req, res, next) {
-    res.render('posts')
+    const {author} = req.query
+    PostModel.getPosts(author)
+        .then(result => {
+            res.render('posts', {
+                posts: result
+            })
+        })
+        .catch(next)    
   })
   
   // POST /posts/create 发表一篇文章
   router.post('/create', checkLogin, function (req, res, next) {
-    res.send('发表文章')
+    const {title, content,} = req.fields
+    const author = req.session.user._id
+    
+    // 数据校验
+    try {
+        if (!title) {
+            throw new Error('请输入标题')
+        }
+        if (!content) {
+            throw new Error('请输入内容')
+        }
+    } catch (e) {
+        req.flash('error', e.message)
+        return res.redirect('back')
+    }
+    let post = {title, content, author}
+    PostModel.create(post)
+        .then(result => {
+            post = result.ops[0]
+            req.flash('success', '发表成功')
+            res.redirect(`/posts/${post._id}`)
+        })
+        .catch(next)
   })
   
   // GET /posts/create 发表文章页
@@ -19,7 +50,18 @@ router.get('/', function (req, res, next) {
   
   // GET /posts/:postId 单独一篇的文章页
   router.get('/:postId', function (req, res, next) {
-    res.send('文章详情页')
+    const {postId} = req.params
+    Promise.all([
+        PostModel.getPostById(postId),
+        PostModel.incPv(),   // pv + 1
+    ]).then(result => {
+        const [post] = result
+        if (!post) {
+            req.flash('error', '文章不存在')
+            return res.redirect('/posts')
+        }
+        res.render('post', { posts: [post] })
+    }).catch(next)
   })
   
   // GET /posts/:postId/edit 更新文章页
